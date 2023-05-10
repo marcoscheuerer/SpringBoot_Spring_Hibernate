ade0c3bbc891cb11ef6b49dda9ede04591b746f3
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-05-10 08:39:41 +0200

    Springboot Overview

---
+++ 01-springboot-overview/01-spring-boot-demo/src/main/java/mao/linatrix/sprintboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,13 @@
+package mao.linatrix.sprintboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+}
